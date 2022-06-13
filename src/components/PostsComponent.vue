800c2da2a485a2aabcb1af09c222a9aef9625b4f
Mustafa Mwafy <66922777+mwafy092@users.noreply.github.com>
2022-06-13 08:25:00 +0200

    adding fetch function using axios

---
+++ src/components/PostsComponent.vue
@@ -0,0 +1,49 @@
+<script setup>
+import axios from "axios";
+import { reactive } from "vue";
+
+let state = reactive({
+  posts: [],
+});
+function fetchPosts(n) {
+  axios
+    .get(`https://jsonplaceholder.typicode.com/posts?_page=1&_limit=${n}`)
+    .then((res) => (state.posts = res.data));
+}
+</script>
+
+<template>
+  <button @click="fetchPosts">fetch posts</button>
+  <div class="posts-container">
+    <div class="post" v-for="post in state.posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.posts-container {
+  width: 70vw;
+  margin: 10px auto;
+}
+.post {
+  background: rgb(24, 146, 24);
+  color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
+    rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.post h2 {
+  font-size: 20px;
+  color: #333;
+}
+
+.post p {
+  font-size: 18px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-createApp(App).mount('#app')
+createApp(App).use(VueAxios, axios).mount("#app");
